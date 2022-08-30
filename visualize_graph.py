121e94aab8218bddc1a9bd333e3aa3cad43f0de4
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2022-08-30 16:26:02 +0900

    feat paper setting&line plot& scatter

--- visualize_graph.py
+++ visualize_graph.py
@@ -8,6 +8,45 @@ from scipy import signal
 import numpy as np
 
 
+def scatter_point(data1,data2=None,data3=None):
+    fig = plt.figure(figsize=(9,6))
+    ax = fig.add_subplot(111,projection='3d')
+    ax.scatter(data1[:,0],data1[:,1],data1[:,2],color='r',alpha = 0.5)
+    if data2 is not None :
+        ax.scatter(data2[:, 0], data2[:, 1], data2[:, 2],color='g' ,alpha=0.5)
+    if data3 is not None :
+        ax.scatter(data3[:, 0], data3[:, 1], data3[:, 2],color='b' ,alpha=0.5)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
+
+def plot_line(data1, data2=None):
+    fig = plt.figure(figsize=(9, 6))
+    ax = fig.add_subplot(111, projection='3d')
+
+    N = data1.shape[0]
+    for i in range(N):
+        ax.plot([0,data1[i,0]],[0,data1[i,1]], [0,data1[i,2]], color='r', linewidth=0.5, alpha=0.5)
+
+    if data2 is not None:
+        N = data2.shape[0]
+        for i in range(N):
+            ax.plot([0, data2[i, 0]], [0, data2[i, 1]], [0, data2[i, 2]], color='b', linewidth=0.5, alpha=0.5)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
+
+    # png_fname = "{}/{}.png".format(path,ep)
+    # plt.savefig(png_fname,dpi=75)
+    # clean up
+    # plt.clf()
+
+
+
 flg2 = plt.figure()      # 차트 플롯 생성
 chart = flg2.add_subplot(1, 1, 1)    # 행, 열, 위치
 
